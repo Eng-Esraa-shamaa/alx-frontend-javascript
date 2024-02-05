4fff1e33bed828331eeb1e27043d0a71d106d2f9
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2024-02-05 13:23:02 +0200

    .

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+export default function createIteratorObject(report) {
+  let allEmployees = [];
+  for (const emp of Object.values(report.allEmployees)) {
+    allEmployees = [
+      ...allEmployees,
+      ...emp,
+    ];
+  }
+  return allEmployees;
+}
