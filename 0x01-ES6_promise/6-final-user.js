166a0a3a160fa02c97bad102516ebb8f089047e3
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2024-02-06 17:44:22 +0200

    modify

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,12 +4,22 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((result) => {
-    const array = [];
-    for (const i of result) {
-      array.push({ status: i.status, value: i.value || i.reason });
-    }
-    return array;
-  });
+    uploadPhoto(fileName)])
+    .then((result) => {
+      const array = [];
+      result.forEach((index) => {
+        if (index.status === 'fulfilled') {
+          array.push({
+            status: index.status,
+            value: index.value,
+          });
+        } else {
+          array.push({
+            status: index.status,
+            value: `Error: ${index.reason.message}`,
+          });
+        }
+      });
+      return array;
+    });
 }
