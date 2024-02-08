f5dc2baf62eaa2b235c608fd5ffe48b095c16149
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2024-02-08 11:15:27 +0200

    .

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,18 +3,23 @@ export default class Currency {
     this.code = code;
     this.name = name;
   }
+
   get code() {
-    this.code = _code;
+    return this._code;
   }
+
   set code(codeType) {
     this._code = codeType;
   }
+
   get name() {
-    this.name = _name;
+    return this._name;
   }
+
   set name(nameType) {
     this._name = nameType;
   }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
