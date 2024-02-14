b93e72f2226eae98e3734ce532590a69243c7ae0
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2024-02-14 13:30:02 +0200

    task 0 typescript

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,32 @@
+interface Student {
+	firstName: string,
+	lastName: string,
+	age: number,
+	location: string,
+}
+const student1: Student = {
+	firstName: "Esraa",
+	lastName: "yehia",
+	age: 30,
+	location: "egypt"
+};
+const student2: Student = {
+        firstName: "Asma",
+        lastName: "Ibrahim",
+        age: 8,
+        location: "egypt"
+};
+const studentsList: Student[] = [student1, student2];
+function renderTable() {
+  const table = document.createElement("table");
+  studentsList.forEach(student => {
+    const row = table.insertRow();
+    const firstNameCell = row.insertCell();
+    firstNameCell.innerHTML = student.firstName;
+    const locationCell = row.insertCell();
+    locationCell.innerHTML = student.location;
+  });
+  document.body.appendChild(table);
+}
+
+renderTable();
